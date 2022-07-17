daf301c0fb10e3b0cc11bebe272f9a76e1cdd635
Hafif <hafifimammuddyn@gmail.com>
2022-07-17 22:46:28 +0700

    ♻️ focus on get and send data with fake api

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, ScrollView } from 'react-native';
 import FlexBox from './pages/FlexBox';
 import Position from './pages/Position';
 import SampleComponent from './pages//SampleComponent';
 import StylingComponent from './pages/StylingComponent';
+import CallAPIVanilla from './pages/CallAPIVanilla';
 
 const App = () => {
+  const [isShow, setIsShow] = useState(true);
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setIsShow(false);
+  //   }, 6000);
+  // }, []);
+
   return (
     <View>
       <ScrollView>
-        <StylingComponent />
-        <SampleComponent />
-        <FlexBox />
-        <Position />
+        {/* <StylingComponent />
+        <SampleComponent /> */}
+        {/* {isShow && <FlexBox />} */}
+        {/* <Position /> */}
+        <CallAPIVanilla />
       </ScrollView>
     </View>
   );
